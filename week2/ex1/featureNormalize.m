9edf6d437a6e8b0fe30d3c4150a96df328f59f8b
Boris Tsarev <btsarev@17bullets.com>
2018-04-04 23:44:12 +0300

    Required week2 assignments

--- week2/ex1/featureNormalize.m
+++ week2/ex1/featureNormalize.m
@@ -5,10 +5,9 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 %   is 1. This is often a good preprocessing step to do when
 %   working with learning algorithms.
 
-% You need to set these values correctly
-X_norm = X;
-mu = zeros(1, size(X, 2));
-sigma = zeros(1, size(X, 2));
+mu = mean(X, 1)';
+sigma = std(X, 0, 1)';
+
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: First, for each feature dimension, compute the mean
