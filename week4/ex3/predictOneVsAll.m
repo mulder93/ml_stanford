cd33cbe65419f6cf03b712db94662bc1845c33b8
Boris Tsarev <btsarev@17bullets.com>
2018-04-13 00:26:20 +0300

    Did week4 exersizes

--- week4/ex3/predictOneVsAll.m
+++ week4/ex3/predictOneVsAll.m
@@ -17,6 +17,8 @@ p = zeros(size(X, 1), 1);
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
 
+[prob, p] = max(sigmoid(X * all_theta'), [], 2);
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
 %               your learned logistic regression parameters (one-vs-all).
@@ -29,14 +31,6 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
-
-
-
-
-
-
-
 % =========================================================================
-
 
 end
